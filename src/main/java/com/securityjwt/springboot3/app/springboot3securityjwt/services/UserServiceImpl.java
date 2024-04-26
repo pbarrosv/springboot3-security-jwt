99dbbf8f2ee8066a54573a0b3fb8b2bfbcfdfcb8
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-26 18:48:11 -0500

    Configurando el metodo save() de Usuario y codificando el password

--- src/main/java/com/securityjwt/springboot3/app/springboot3securityjwt/services/UserServiceImpl.java
+++ src/main/java/com/securityjwt/springboot3/app/springboot3securityjwt/services/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.securityjwt.springboot3.app.springboot3securityjwt.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.securityjwt.springboot3.app.springboot3securityjwt.entities.Role;
 import com.securityjwt.springboot3.app.springboot3securityjwt.entities.User;
 import com.securityjwt.springboot3.app.springboot3securityjwt.repositories.RoleRepository;
 import com.securityjwt.springboot3.app.springboot3securityjwt.repositories.UserRepository;
@@ -30,6 +33,20 @@ public class UserServiceImpl implements UserService{
 	@Override
 	@Transactional
 	public User save(User user) {
+		Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+		List<Role> roles = new ArrayList<>();
+		//optionalRoleUser.ifPresent(role -> roles.add(role));
+		optionalRoleUser.ifPresent(roles::add);
+		
+		if(user.isAdmin()) {
+			Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+			optionalRoleAdmin.ifPresent(roles::add);
+		}
+		
+		user.setRoles(roles);
+		
+		String passwordEncoded = passwordEncoder.encode(user.getPassword());
+		user.setPassword(passwordEncoded);
 		
 		return repositoryU.save(user);
 	}
