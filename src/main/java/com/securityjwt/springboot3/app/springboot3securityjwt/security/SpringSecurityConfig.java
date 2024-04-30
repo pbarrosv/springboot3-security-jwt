0d8c1e13b95445d5be2defcdfb35e16a84db7138
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-30 10:46:42 -0500

    Se da permisos al metodo Post register()

--- src/main/java/com/securityjwt/springboot3/app/springboot3securityjwt/security/SpringSecurityConfig.java
+++ src/main/java/com/securityjwt/springboot3/app/springboot3securityjwt/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.securityjwt.springboot3.app.springboot3securityjwt.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,7 +21,8 @@ public class SpringSecurityConfig {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http.authorizeHttpRequests( authz -> authz
-				.requestMatchers("/api/users").permitAll()/*Permite el acceso solo a /users*/
+				.requestMatchers(HttpMethod.GET,"/api/users").permitAll()/*Permite el acceso solo a /users*/
+				.requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
 				.anyRequest().authenticated())/*para todo lo demas necesita autentificacion*/
 				.csrf(config -> config.disable()) /*Solo es para vistas como thymeaf jsp*/
 				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))/*para que la sesion http no tenga estado y todo que es autentificacion se maneje en el Token*/
